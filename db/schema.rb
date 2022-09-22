6d8e499be799ef0082a4111a976003c60d2a8e62
Nakayama1616 <shohei.nakayama@finc.com>
2022-09-22 19:16:12 +0900

    保存

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_22_061148) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_100614) do
   create_table "comments", charset: "utf8mb4", force: :cascade do |t|
     t.string "body"
     t.bigint "my_thread_id", null: false
@@ -23,6 +23,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_22_061148) do
     t.string "tittle"
     t.text "body"
     t.datetime "created_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "my_threads"
